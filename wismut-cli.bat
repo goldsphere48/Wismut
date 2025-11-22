d5d6e417241639f6eff65b7d1eb0f62e62e96387
Viktor Titov <goldsphere48@gmail.com>
2025-11-22 19:50:12 +1000

    Fix wismut-cli building.

--- wismut-cli.bat
+++ wismut-cli.bat
@@ -1,3 +1,10 @@
-cmake %~dp0 --preset=windows-release
+set "WISMUT_ROOT=%~dp0"
+set "CALL_LOCATION=%cd%"
+pushd %WISMUT_ROOT%
+
+cmake --preset=windows-release
 cmake --build --preset=windows-release
-%~dp0\Engine\Tools\CLI\bin\Release\wismut-cli.exe --wismut-path=%~dp0 --location=%cd% %*
+
+popd
+
+%~dp0Engine\Tools\CLI\bin\Release\Windows\wismut-cli.exe --wismut-path=%~dp0 --location=%CALL_LOCATION% %*
